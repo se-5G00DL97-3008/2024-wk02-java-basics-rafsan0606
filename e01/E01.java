a358fef466a43e8de4d8ceb77611af589abe1d95
rafsan0606 <148779743+rafsan0606@users.noreply.github.com>
2024-01-17 22:48:30 +0300

    Update E01.java

--- e01/E01.java
+++ e01/E01.java
@@ -1,5 +1,21 @@
 class E01 {
     public static void main(String[] args) {
+        int x = 10;  
+        float y = (float) 5.5;
+        boolean flag = true;
+        char letter = 'c';
+        short year = 2022;
+        long population = 7000000000L;
+        byte num = 100;
+        double pi = 3.13457599923384753929348;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(flag);
+        System.out.println(letter);
+        System.out.println(year);
+        System.out.println(population);
+        System.out.println(num);
+        System.out.println(pi);
 
     }
 }
