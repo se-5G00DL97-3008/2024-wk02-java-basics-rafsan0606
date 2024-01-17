aad33b6eef0d5d9812e03c8b7505433e3d04505a
rafsan0606 <148779743+rafsan0606@users.noreply.github.com>
2024-01-18 00:28:29 +0300

    Update E08.java

--- e08/E08.java
+++ e08/E08.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 class E08 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter a number (0 to quit):");
+            int number = scanner.nextInt();
+
+            if (number == 0) {
+                break;
+            }
+
+            if (number % 2 == 0) {
+                System.out.println("Even number");
+            } else {
+                System.out.println("Odd number");
+            }
+        }
+
+        scanner.close();
 
     }
 }
