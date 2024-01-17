06af09ef92cc057ca3290e62dfc254b4d04f86d5
rafsan0606 <148779743+rafsan0606@users.noreply.github.com>
2024-01-18 00:09:48 +0300

    Update E06.java

--- e06/E06.java
+++ e06/E06.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 class E06 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Which multiplication table to show?");
+        int inputNumber = scanner.nextInt();
+
+        for (int i = 0; i <= 10; i++) {
+            int result = i * inputNumber;
+            System.out.println(i + " x " + inputNumber + " = " + result);
+        }
+
+        scanner.close();
 
     }
 }
