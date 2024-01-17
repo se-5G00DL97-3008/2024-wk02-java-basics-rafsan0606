454a5266bf57c103aae0b61ee5e57f1e9e2bdffc
rafsan0606 <148779743+rafsan0606@users.noreply.github.com>
2024-01-17 22:57:35 +0300

    Update E02.java

--- e02/E02.java
+++ e02/E02.java
@@ -1,5 +1,23 @@
 class E02 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Give a number:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Give another number:");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+        int multiplication = num1 * num2;
+        int substraction = num1 - num2;
+        int division = num1 / num2;
+
+        System.out.println(num1 + " + " + num2 + " = " + sum);
+        System.out.println(num1 + " * " + num2 + " = " + multiplication);
+        System.out.println(num1 + " - " + num2 + " = " + substraction);
+        System.out.println(num1 + " / " + num2 + " = " + division);
+
+        scanner.close();
     }
 }
