6dbad45a8b512df863510087e8d4766d4fff8e10
rafsan0606 <148779743+rafsan0606@users.noreply.github.com>
2024-01-17 23:39:39 +0300

    Update E03.java

--- e03/E03.java
+++ e03/E03.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 class E03 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        double number = scanner.nextDouble();
+
+        if (number > 0) {
+            System.out.println("Positive number");
+        } else if (number < 0) {
+            System.out.println("Negative number");
+        }
+
+        scanner.close();
 
     }
 }
