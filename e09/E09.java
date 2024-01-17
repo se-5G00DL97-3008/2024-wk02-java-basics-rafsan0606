6f0b2e791db150a6bbe234a578dd88c56960e508
rafsan0606 <148779743+rafsan0606@users.noreply.github.com>
2024-01-18 00:38:17 +0300

    Update E09.java

--- e09/E09.java
+++ e09/E09.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 class E09 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int total = 0;
+        int count = 0;
+
+        while (true) {
+            System.out.println("Give a test score (-1 to quit):");
+            int score = scanner.nextInt();
+
+            if (score == -1) {
+                break;
+            }
+
+            total += score;
+            count++;
+
+            double average = (double) total / count;
+            System.out.println("Average: " + average);
+        }
+
+        scanner.close();
 
     }
 }
